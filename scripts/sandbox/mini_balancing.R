2337861b502887b85a9a3f308ff151afec349555
Emma Sofie Skarstein <emma@skarstein.no>
2025-08-19 12:53:23 +0200

    Create mini_balancing.R

---
+++ scripts/sandbox/mini_balancing.R
@@ -0,0 +1,47 @@
+# Simulated example
+mu_v <- MASS::rnegbin(6, mu = 1000, theta = 1)
+mu_v
+Sigma_v <- diag(rnorm(6)^2)
+Sigma_v
+
+
+A1 <- matrix(c( 1,-1, 0, 0,-1, 1,
+               -1, 1, 1,-1, 0, 0,
+                0, 0,-1, 1, 1,-1), 
+             byrow = TRUE, nrow = 3)
+A1
+
+det(A1 %*% t(A1))
+
+Sigma_b <- A1 %*% Sigma_v %*% t(A1)
+
+det(Sigma_b)
+
+b <- rep(0, 3)
+
+
+
+# 1. Pseudoinverse
+Sigma_b_inv <- MASS::ginv(Sigma_b)
+mu_v_given_b_1 <- mu_v + Sigma_v %*% t(A1) %*% Sigma_b_inv %*% (0 - A1 %*% mu_v)
+Sigma_v_given_b_1 <- Sigma_v + Sigma_v %*% t(A1) %*% Sigma_b_inv %*% t(Sigma_v %*% t(A1))
+
+
+# 2. Remove one constraint
+A1_red <- A1[-1, ]
+Sigma_b_red <- A1_red %*% Sigma_v %*% t(A1_red)
+Sigma_b_inv <- solve(Sigma_b_red)
+mu_v_given_b_2 <- mu_v + Sigma_v %*% t(A1_red) %*% Sigma_b_inv %*% (0 - A1_red %*% mu_v)
+Sigma_v_given_b_2 <- Sigma_v + Sigma_v %*% t(A1_red) %*% Sigma_b_inv %*% t(Sigma_v %*% t(A1_red))
+
+
+# 3. Regularization
+Sigma_b_inv <- solve(Sigma_b + diag(rep(1e-6, 3)))
+mu_v_given_b_3 <- mu_v + Sigma_v %*% t(A1) %*% Sigma_b_inv %*% (0 - A1 %*% mu_v)
+Sigma_v_given_b_3 <- Sigma_v + Sigma_v %*% t(A1) %*% Sigma_b_inv %*% t(Sigma_v %*% t(A1))
+
+
+# Compare the three solutions
+cbind(mu_v_given_b_1, mu_v_given_b_2, mu_v_given_b_3)
+
+
