9e9ae384309e7564e09c959ff7dbfc825e78458f
Emma Sofie Skarstein <emma@skarstein.no>
2025-09-02 10:49:53 +0200

    switch to new balancing

--- scripts/sandbox/aalesund.R
+++ scripts/sandbox/aalesund.R
@@ -12,12 +12,13 @@ source("R/model_validation.R")
 
 aalesund_data <- read_sf("data/processed/aalesund_data.geojson")
 aadt2024 <- read.csv("data/raw/traffic-links-aadt-data-2024.csv")
+nodes <- read_sf("data/raw/traffic-nodes-2024.geojson")
 
 # Create spatial index - this is simply the row number for each traffic link
 aalesund_data$spatial.idx <- 1:nrow(aalesund_data)
 
 adj_sparse <- build_adjacency_matrix(aalesund_data)
-constraint_matrix <- build_flow_constraints(aalesund_data)
+constraint_matrix <- build_incidence_matrix(nodes, link_ids= aalesund_data)
 
 
 formula <- aadt ~ minLanes +
@@ -54,6 +55,15 @@ rbind(inla_res$approved, balanced_res$approved)
 
 
 
+nvdb <- nvdb_objects()
+
+pal <- leaflet::colorBin(
+  palette = "viridis",
+  domain = NULL, 
+  reverse = TRUE,
+  na.color = "#88807b"
+)
+
 retta <- inla_res$retta %>% 
   st_as_sf() %>% 
   mutate(balansert_pred = balanced_res$retta$balansert_pred,
@@ -70,3 +80,15 @@ leaflet::leaflet(retta, options = leaflet::leafletOptions(crs = nvdb$nvdb_crs, z
     color = ~ pal(retta[["balansert_pred"]]),
     popup = ~ text,
     opacity = 1)
+
+
+
+examine_node_flow(A1 = constraint_matrix, node_id = "271925")
+turns_df <- get_turning_movements(nodes = nodes, node_id = "271925")
+turns_df[1,2]
+
+print_turning_movements_for_link_at_node(node_id = "271925", link_id = "0.0-1.0@248954-WITH", nodes)
+print_turning_movements_for_link_at_node(node_id = "271123", link_id = "0.08626678-1.0@248963-WITH", nodes)
+print_turning_movements_for_link_at_node(node_id = "271111", link_id = "0.85695107-0.78147169@248950-WITH", nodes)
+
+get_turning_movements(nodes, "271111")
