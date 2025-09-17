33c7a4fd520de5531652c94f1860e7f3043e2755
Emma Sofie Skarstein <emma@skarstein.no>
2025-09-17 10:45:54 +0200

    clean up

--- scripts/sandbox/county_model.R
+++ scripts/sandbox/county_model.R
@@ -9,9 +9,6 @@ source("R/utilities.R")
 source("R/model_fitting.R")
 source("R/model_validation.R")
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-# Load data and matrices ----
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
 
 data <- readRDS("data/processed/engineered_data.rds")
 aadt2024 <- load_data(config$data_paths$raw$aadt_results)
@@ -61,7 +58,6 @@ balanced_res <- calculate_approved(
 rbind(inla_res$approved, balanced_res$approved)
 
 
-
 retta <- inla_res$retta %>% 
   add_geometry_to_traffic_links() %>% 
   st_as_sf() %>% 
@@ -91,82 +87,3 @@ leaflet::leaflet(retta, options = leaflet::leafletOptions(crs = nvdb$nvdb_crs, z
     opacity = 1)
 
 
-
-
-# Testing functions
-
-print_turning_movements_for_link_at_node(node_id = "347086", link_id = "0.0-1.0@319629-WITH", nodes)
-
-turns <- get_turning_movements(nodes, node_id = "271925")
-
-
-
-#model <- fit_model(data, c("minLanes", "functionalRoadClass"))
-model_county <- fit_model(county_data, c("minLanes", "functionalRoadClass"))
-
-national_res <- fit_national_model(data, 
-                                   formula_covariates = c("minLanes", "functionalRoadClass"),
-                                   grouping_variable = "county", 
-                                   groups = unique(data$county), 
-                                   inla_scope = "local")
-
-res <- run_modeling_pipeline(groups_to_process = c("Telemark", "Buskerud"))
-
-
-retta <- res$data %>% 
-  add_geometry_to_traffic_links() %>% 
-  #st_as_sf() %>% 
-  mutate(text = paste0("INLA: ", pred, 
-                       "<br>Balanced: ", balanced_pred,
-                       #"<br>ÅDT 2023: ", ÅDT.fjorårets,
-                       "<br>Målt eller utleda ÅDT: ", aadt,
-                       "<br>ID: ", id))
-nvdb <- nvdb_objects()
-
-pal <- leaflet::colorBin(
-  palette = "viridis",
-  domain = NULL, 
-  reverse = TRUE,
-  na.color = "#88807b"
-)
-
-leaflet::leaflet(retta, options = leaflet::leafletOptions(crs = nvdb$nvdb_crs, zoomControl = TRUE)) |>
-  leaflet::addTiles(urlTemplate = nvdb$nvdb_url, attribution = nvdb$nvdb_attribution)  |>
-  leaflet::addPolylines(
-    color = ~ pal(retta[["balanced_pred"]]),
-    popup = ~ text,
-    opacity = 1)
-
-
-balanced_i_intersections <- run_modeling_pipeline(groups_to_process = "all", 
-                                                  balance_i_intersections = TRUE)
-unbalanced_i_intersections <- run_modeling_pipeline(groups_to_process = "all", 
-                                                    balance_i_intersections = FALSE)
-
-
-good_formula <- c("functionalRoadClass:maxLanes", 
-                  "minLanes:roadCategory", 
-                  "functionalRoadClass:roadCategory", 
-                  "functionalRoadClass", 
-                  "maxLanes", 
-                  "minLanes", 
-                  "roadCategory", 
-                  "hasOnlyPublicTransportLanes", 
-                  "isFerryTrafficLink", 
-                  "hasTouristRoad")
-
-good_formula_merged <- c("functionalRoadClass_merged:maxLanes_merged", 
-                  "minLanes_merged:roadCategory", 
-                  "functionalRoadClass_merged:roadCategory", 
-                  "functionalRoadClass_merged", 
-                  "maxLanes_merged", 
-                  "minLanes_merged", 
-                  "roadCategory", 
-                  "hasOnlyPublicTransportLanes", 
-                  "isFerryTrafficLink", 
-                  "hasTouristRoad")
-
-merged_covariates <- run_modeling_pipeline(groups_to_process = "Trøndelag", 
-                                           covariates = good_formula_merged)
-original_covariates <- run_modeling_pipeline(groups_to_process = "Trøndelag",
-                                             covariates = good_formula)
