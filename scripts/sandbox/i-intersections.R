73574449115917fde6a5c68217eaa4c0a3a8829f
Emma Skarstein <emma@skarstein.no>
2025-09-22 15:51:13 +0200

    Update i-intersections.R

--- scripts/sandbox/i-intersections.R
+++ scripts/sandbox/i-intersections.R
@@ -13,12 +13,20 @@ source("R/model_fitting.R")
 
 nodes <- read_sf("data/raw/traffic-nodes-2024.geojson")
 
+nodes$number_of_traffic_links <- lengths(nodes$connectedTrafficLinkIds)
+nodes$number_of_candidate_links <- lengths(nodes$connectedTrafficLinkCandidateIds)
 
 two_in_two_out <- dplyr::filter(nodes, numberOfIncomingLinks == 2 & numberOfOutgoingLinks == 2)
 two_parent_links <- dplyr::filter(two_in_two_out, numberOfUndirectedLinks == 2)
+unused_candidates <- dplyr::filter(two_parent_links, number_of_candidate_links > number_of_traffic_links)
 
+nrow(two_in_two_out)
+nrow(two_parent_links)
+nrow(unused_candidates)
 
-leaflet::leaflet(two_parent_links, options = leaflet::leafletOptions(crs = nvdb$nvdb_crs, zoomControl = TRUE)) |>
+used_all_candidates <- dplyr::filter(two_parent_links, number_of_candidate_links == number_of_traffic_links)
+
+leaflet::leaflet(used_all_candidates, options = leaflet::leafletOptions(crs = nvdb$nvdb_crs, zoomControl = TRUE)) |>
   leaflet::addTiles(urlTemplate = nvdb$nvdb_url, attribution = nvdb$nvdb_attribution)  |>
   leaflet::addCircleMarkers()
 
